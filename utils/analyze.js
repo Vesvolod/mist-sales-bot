68c1ec96584d53bc4135c905c9574c3047d5c226
Vesvolod <sevabeliy@gmail.com>
2025-03-26 17:14:58 +0800

    Create analyze.js

---
+++ utils/analyze.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export async function analyzeMessage(text) {
+  const res = await axios.post("https://mist-chat-widget.vercel.app/api/analyze", {
+    message: text
+  });
+
+  return res.data;
+}
